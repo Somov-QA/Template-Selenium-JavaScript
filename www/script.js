3778cad76d458f7a9af187eb8ba0d08daa698c5d
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-01-09 10:22:15 +0300

    Добавлен простой функцилонал для тестирования авторизации

---
+++ www/script.js
@@ -0,0 +1,16 @@
+var Events = (function() {
+    console.log("Модуль подключен");
+    return {
+        onButtonClick: function() {
+            var username = document.getElementById('username');
+            var password = document.getElementById('password');
+            var result = document.getElementById('result');
+            console.log("Проверка авторизации: ", username.value, password.value);
+            if(username.value == "admin" && password.value == "123"){
+                result.innerHTML = "Correct";
+            }else{
+                result.innerHTML = "Incorrect";
+            }
+        }
+    }
+}());
